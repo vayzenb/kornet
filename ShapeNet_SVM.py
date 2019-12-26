28f2c51f2f5ec16bc4e79081a0559bce511ae666
vayzenb <vayzenb@emory.edu>
2019-12-26 14:31:39 -0500

    working version

--- ShapeNet_SVM.py
+++ ShapeNet_SVM.py
@@ -21,6 +21,7 @@ from ShapeNet import get_vector
 
 #cond = ['Outline', 'Pert', 'IC',  'Outline_Black', 'Pert_Black', 'IC_Black', 'Outline_Black_Filled', 'Pert_Black_Filled']
 cond = ['Outline', 'Pert', 'IC']
+
 suf = ['', '_ripple', '_IC', '', '_ripple', '_IC','', '_ripple']
 
 KN=pd.read_csv('KN_Classes.csv', sep=',',header=None).to_numpy() 
@@ -29,20 +30,17 @@ layer = "avgpool"
 model = load_model() 
 model.eval() #Set model into evaluation mode
 
-IMName = "Stim/Training/All/Dog/Dog_4 (2).jpg"
+trK = 20 #Number of training images to use
+folK = 5 #Number of folds over the training set
 
-vec = get_vector(IMName, model, layer).numpy()
-
-train_labels = [np.repeat(1, 20).tolist(), np.repeat(2, 20).tolist()]
+train_labels = [np.repeat(1, trK).tolist(), np.repeat(2, trK).tolist()]
 train_labels = list(chain(*train_labels))
 test_labels = [1,2]
 
-trK = 20 #Number of training images to use
-folK = 3 #Number of folds over the training set
-
+ShapeNet_Acc = np.empty((210, len(cond) + 2), dtype = object)
 for ii in range(0, len(cond)): 
     AllActs = {"Train" : np.zeros((trK*2, 2048)), "Test" : np.zeros((2, 2048))}
-    
+    n = 0
     #Loop through each possible image combination in a condition 
     for kk in range(0, len(KN)):
         #load first image
@@ -59,27 +57,43 @@ for ii in range(0, len(cond)):
                 AllActs["Test"][1] = get_vector(IM2, model, layer).numpy()
                 imList2 = [os.path.basename(x) for x in glob.glob("Stim/Training/" + KN[jj][0] + "/*.jpg")]
                 
+                tempScore = 0 
                 for fl in range(0,folK): #loop through folds
                     #randomize order of image lists every iteration
                     imList1 = random.sample(imList1, len(imList1))
                     imList2 = random.sample(imList2, len(imList2))
                     for tr in range(0,trK): #loop through training images
-                        trIM1= "Stim/Training/" + KN[kk][0] + "/" + imList1[tr]
-                        trIM2= "Stim/Training/" + KN[jj][0] + "/" + imList2[tr]
-                        
-                        #Extract features for each training image
-                        AllActs["Train"][tr] = get_vector(trIM1, model, layer).numpy()
-                        AllActs["Train"][tr+20] = get_vector(trIM2, model, layer).numpy()
+                        try:
+                            trIM1= "Stim/Training/" + KN[kk][0] + "/" + imList1[tr]
+                            trIM2= "Stim/Training/" + KN[jj][0] + "/" + imList2[tr]
+                            
+                            #Extract features for each training image
+                            AllActs["Train"][tr] = get_vector(trIM1, model, layer).numpy()
+                            AllActs["Train"][tr+trK] = get_vector(trIM2, model, layer).numpy()
+                        except: #If there is some error with an image try next image outside the set
+                            trIM1= "Stim/Training/" + KN[kk][0] + "/" + imList1[tr+trK] #add number of training images to try out of set image
+                            trIM2= "Stim/Training/" + KN[jj][0] + "/" + imList2[tr+trK]
+                            
+                            #Extract features for each training image
+                            AllActs["Train"][tr] = get_vector(trIM1, model, layer).numpy()
+                            AllActs["Train"][tr+trK] = get_vector(trIM2, model, layer).numpy()
                         
                     #Run SVM
                     clf = svm.SVC(kernel='linear', C=1).fit(AllActs["Train"], train_labels)
-                    clf.score(AllActs["Test"], test_labels)
-                    
-                        
-                        
+                    #Add current score to existing
+                    tempScore = tempScore + clf.score(AllActs["Test"], test_labels)
+                
+                ShapeNet_Acc[n,0] = KN[kk][0]
+                ShapeNet_Acc[n,1] = KN[jj][0]
+                ShapeNet_Acc[n,ii+2] = tempScore/folK
+                
+                print(np.round((n/210)*100,decimals = 2), KN[kk][0], KN[jj][0], tempScore/folK)
+                n = n +1
                 
             else: #move to next iteration
                 continue
+
+    np.savetxt('Results/ShapeNet_SVM.csv', ShapeNet_Acc, delimiter=',', fmt= '%s')
             
             
         
