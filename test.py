07052f52d6891d79a528a43e573573ca589efeab
vayzenb <vayzenberg90@gmail.com>
2020-05-17 09:55:35 -0400

    test

--- test.py
+++ test.py
@@ -5,17 +5,4 @@ Created on Wed Dec 25 11:59:59 2019
 @author: VAYZENB
 """
 
-import os
-
-os.chdir('C:/Users/vayzenb/Desktop/GitHub Repos/KorNet/')
-
-from ShapeNet_SVM as
-
-pic_one = str("Stim/Training/Dog/Dog_4 (2).jpg")
-pic_two = str("Stim/Training/All/Dog/Dog_4 (2).jpg")
-
-
-
-#pic_one_vector = get_vector(pic_two)
-#vec = pic_one_vector.numpy()
-#pic_two_vector = get_vector(pic_two)
+newTest
